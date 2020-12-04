6ca72b334e1cea64d626d4b8b3a8e0e4084b0efe
dselivanovskaya <selivanovskaya.dasha@mail.ru>
2020-12-04 12:06:45 +0300

    database creation function added

--- procedures.sql
+++ procedures.sql
@@ -1,3 +1,24 @@
+CREATE EXTENSION dblink;
+-- Creating a database by name --
+--- SELECT CreateDB('app'); --
+CREATE OR REPLACE FUNCTION CreateDB(dbname text)
+RETURNS integer AS
+$func$
+BEGIN
+
+IF EXISTS (SELECT 1 FROM pg_database WHERE datname = dbname) THEN
+	RAISE NOTICE 'Database already exists';
+	RETURN 1;
+ELSE
+	PERFORM dblink_exec('dbname=' || current_database() || ' user=postgres password=mypass' 
+, 'CREATE DATABASE ' || quote_ident(dbname));
+	RETURN 1;
+END IF;
+
+END
+$func$ LANGUAGE plpgsql;
+
+
 -- CALL CreateTables();
 CREATE OR REPLACE PROCEDURE CreateTables()
 LANGUAGE plpgsql
