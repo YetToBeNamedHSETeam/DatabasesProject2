5638324b36ea3a8c7f0c8d9a3daea44f21013f7f
katyamalysheva <55476682+katyamalysheva@users.noreply.github.com>
2020-12-13 19:23:14 +0300

    Update create_database.sql
    added function DeleteDB

--- sql/create_database.sql
+++ sql/create_database.sql
@@ -20,3 +20,26 @@ END IF;
 
 END
 $func$ LANGUAGE plpgsql;
+				    
+				    
+CREATE OR REPLACE FUNCTION DeleteDB(dbname text)
+-- Deleting a database by name --
+--- SELECT DeleteDB('app');
+RETURNS integer AS
+$func$
+BEGIN
+
+IF EXISTS (SELECT 1 FROM pg_database WHERE datname = dbname) THEN
+	PERFORM dblink_exec('dbname=' || current_database() || ' user=postgres password=mypass'
+, 'DROP DATABASE ' || quote_ident(dbname));
+	IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = dbname) THEN
+	    RAISE NOTICE 'Database deleted successfully';
+    END IF;
+	RETURN 1;
+ELSE
+	RAISE NOTICE 'Database does not exist';
+	RETURN 1;
+END IF;
+
+END
+$func$ LANGUAGE plpgsql; 
